11ccb05f885b30fa84eceedfaabcb934b2dd97f9
RƏBİYYƏ HÜSEYNZADƏ <rebiyyeh19006@sabah.edu.az>
2023-02-05 11:43:29 +0400

    task not completed

---
+++ test.js
@@ -0,0 +1,109 @@
+// // Iterate 10 to 0 using for loop, do the same using while and do while loop
+// let i = 0;
+// while
+//  (i < 10) {
+//   console.log(i);
+//   i++
+// }
+// // Iterate 0 to n using for loop
+
+// // Write a loop that makes the following pattern using console.log():
+// for(var i=1; i<=7; i++){
+//     console.log("* ".repeat(i));
+//   }
+  
+//   //     #
+//   //     ##
+//   //     ###
+//   //     ####
+//   //     #####
+//   //     ######
+//   //     #######
+//   // Use loop to print the following pattern:
+//   for(var i=0; i<=10; i++){
+//       console.log(`${i} * ${i} = ${i * i}`)
+//     }
+//   // 0 x 0 = 0
+//   // 1 x 1 = 1
+//   // 2 x 2 = 4
+//   // 3 x 3 = 9
+//   // 4 x 4 = 16
+//   // 5 x 5 = 25
+//   // 6 x 6 = 36
+//   // 7 x 7 = 49
+//   // 8 x 8 = 64
+//   // 9 x 9 = 81
+//   // 10 x 10 = 100
+//   // Using loop print the following pattern
+//   for(var i=0; i<=10; i++){
+//     console.log(`${i} ${i^2} ${i^3}` );
+//   }
+  
+//   //  i    i^2   i^3
+//   //  0    0     0
+//   //  1    1     1
+//   //  2    4     8
+//   //  3    9     27
+//   //  4    16    64
+//   //  5    25    125
+//   //  6    36    216
+//   //  7    49    343
+//   //  8    64    512
+//   //  9    81    729
+//   //  10   100   1000
+//   // Use for loop to iterate from 0 to 100 and print only even numbers
+//   let i=1 
+//   while (i<=100){
+//     if(i%2==0){
+//     console.log(i)
+//     }
+//     i++
+//   }
+  
+//   // Use for loop to iterate from 0 to 100 and print only odd numbers
+//   let i=1 
+//   while (i<=100){
+//     if(i%2==1){
+//     console.log(i)
+//     }
+//     i++
+//   }
+//   // Use for loop to iterate from 0 to 100 and print only prime numbers
+  
+  
+//   // Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+//   let sum=0;
+//   for (let index = 0; index <= 100; index++) {
+//     sum=sum+ index;
+    
+    
+//   }
+//   console.log(sum);
+  
+//   // Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+//   let even = 0;
+//     let odd = 0;
+//       for (let i = 0; i < n; i++)
+//       {
+       
+//           // Loop to find even, odd sum
+//           if (i % 2 == 0)
+//               even += arr[i];
+//           else
+//               odd += arr[i];
+//       }
+//       console.log(even);
+//       console.log(odd);
+  
+  
+//   // The sum of all evens from 0 to 100 is 2550. And the sum of all odds from 0 to 100 is 2500.
+//   // Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds. Print sum of evens and sum of odds as array
+  
+//   //   [2550, 2500]
+//   // Develop a small script which generate array of 5 random numbers
+  
+//   // Develop a small script which generate array of 5 random numbers and the numbers must be unique
+  
+//   // Develop a small script which generate a six characters random id:
+  
+//   // 5j2khz
